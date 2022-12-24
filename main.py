528b0e7f97769e33c26d15b0a036758d871c7b06
RafaelAlapontGomez <rafael.alapont.gomea@gmail.com>
2022-12-24 12:39:23 +0100

    validación y autenticacióon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from app.v1.router.user_router import router as user_router
 
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return {"message": "Hello World"}
+app.include_router(user_router)
